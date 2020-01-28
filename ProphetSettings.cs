6806b6f65ef077f5ea0ce143d2599039b99efd22
MustBePony <uberskillovikpwnz@yandex.ru>
2020-01-28 08:11:12 +0000

    Code cleared, new feature.
    1. Added color settings.
    2. Added a list with bad prophecies.
    3. Removed logic with the plugin disabled.

--- ProphetSettings.cs
+++ ProphetSettings.cs
@@ -3,24 +3,34 @@ using System.Collections.Generic;
 using ExileCore.Shared.Interfaces;
 using ExileCore.Shared.Nodes;
 using ExileCore.Shared.Attributes;
+using SharpDX;
 
 namespace Prophet
 {
     public class ProphetSettings : ISettings
     {
-        
-        [Menu("General", 101)]
-        public EmptyNode General { get; set; } = new EmptyNode();
 
-        [Menu("Enable Prophet", "yololo", 1001, 101)]
-        public ToggleNode Enable { get; set; } = new ToggleNode(false);
+        public ProphetSettings()
+        {
+            Enable = new ToggleNode(false);
 
-        [Menu("Main Key", "start/stop everything", 1002, 101)]
-        public HotkeyNode StartProphetKey { get; set; } = Keys.Pause;
+            ColorGood = new ColorBGRA(0, 255, 0, 255);
 
+            ColorTrash = new ColorBGRA(0, 0, 255, 255);
+        }
 
+        [Menu("Menu Settings", 100)]
+        public EmptyNode Settings { get; set; }
 
-       
+        [Menu("Enable", parentIndex = 100, Tooltip = "Enable Prophet")]
+        public ToggleNode Enable { get; set; } 
+
+        [Menu("Color good prophecy", parentIndex = 100, Tooltip = "Display color of border good prophecy")]
+        public ColorNode ColorGood { get; set; }
+
+        [Menu("Color trash prophecy", parentIndex = 100, Tooltip = "Display color of border trash prophecy")]
+        public ColorNode ColorTrash { get; set; }
+
 
     }
 }
